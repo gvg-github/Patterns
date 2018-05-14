4890450d0844993dc74510daf3e8538c34a49535
gvg-github <gvg2000@yandex.ru>
2018-05-14 15:28:13 +0400

    Update Controller.java

--- src/ru/geekbrains/pattern/dz7/Controller.java
+++ src/ru/geekbrains/pattern/dz7/Controller.java
@@ -1,6 +1,7 @@
 package ru.geekbrains.pattern.dz7;
 
 import javax.swing.*;
+import javax.swing.table.TableModel;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.sql.ResultSet;
@@ -10,15 +11,9 @@ import java.util.Vector;
 public class Controller implements ActionListener {
 
     private View frame;
-    private Vector column_names;
 
     public Controller(View frame) {
         this.frame = frame;
-        column_names = new Vector();
-        column_names.add("Дата");
-        column_names.add("Сумма");
-        column_names.add("Тип");
-
     }
 
     @Override
@@ -27,14 +22,21 @@ public class Controller implements ActionListener {
 
             ResultSet rs = Model.getData();
             if (rs != null) {
-                Vector db_info = new Vector();
+
+                Vector db_infoRows = new Vector();
                 try {
                     while (rs.next()) {
-                        db_info.add(rs.getDate("Дата"));
+                        Vector db_info = new Vector();
+                        db_info.add(rs.getInt("Номер"));
+                        db_info.add(rs.getString("Дата"));
                         db_info.add(rs.getDouble("Сумма"));
                         db_info.add(rs.getString("Тип"));
+                        db_infoRows.add(db_info);
                     }
-                    frame.setResultTable(new JTable(column_names, db_info));
+                    Vector column_names = frame.getColumn_names();
+                    JTable table = new JTable(db_infoRows, column_names);
+                    TableModel dtm = table.getModel();
+                    frame.getResultTable().setModel(dtm);
                 } catch (SQLException e1) {
                     e1.printStackTrace();
                 }
@@ -43,4 +45,3 @@ public class Controller implements ActionListener {
     }
 
 }
-
