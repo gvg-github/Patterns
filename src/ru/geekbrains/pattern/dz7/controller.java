8e6bf5b70d4e4a4410623117ec83c56784b8420b
gvg-github <gvg2000@yandex.ru>
2018-05-14 13:16:29 +0400

    Update controller.java

--- src/ru/geekbrains/pattern/dz7/controller.java
+++ src/ru/geekbrains/pattern/dz7/controller.java
@@ -1,4 +1,46 @@
-package ru.geekbrains.pattern.dz7;
+package ru.geekbrains.pattern.dz.dz7;
 
-public class controller {
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.Vector;
+
+public class Controller implements ActionListener {
+
+    private View frame;
+    private Vector column_names;
+
+    public Controller(View frame) {
+        this.frame = frame;
+        column_names = new Vector();
+        column_names.add("Дата");
+        column_names.add("Сумма");
+        column_names.add("Тип");
+
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        if (e.getActionCommand().equals("Show results")) {
+
+            ResultSet rs = Model.getData();
+            if (rs != null) {
+                Vector db_info = new Vector();
+                try {
+                    while (rs.next()) {
+                        db_info.add(rs.getDate("Дата"));
+                        db_info.add(rs.getDouble("Сумма"));
+                        db_info.add(rs.getString("Тип"));
+                    }
+                    frame.setResultTable(new JTable(column_names, db_info));
+                } catch (SQLException e1) {
+                    e1.printStackTrace();
+                }
+            }
+        }
+    }
+
 }
+
