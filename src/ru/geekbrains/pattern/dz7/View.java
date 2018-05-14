555104b1f6834f303eeff807f6d797e2a132b9dc
gvg-github <gvg2000@yandex.ru>
2018-05-14 15:29:04 +0400

    Update View.java

--- src/ru/geekbrains/pattern/dz7/View.java
+++ src/ru/geekbrains/pattern/dz7/View.java
@@ -2,24 +2,36 @@ package ru.geekbrains.pattern.dz7;
 
 import javax.swing.*;
 import java.awt.*;
+import java.util.Vector;
 
 public class View extends JFrame {
 
-    private JTable resultTable = new JTable(3, 10);
+    private JTable resultTable;
     private JButton showButton = new JButton("Show results");
+    private final Vector column_names = new Vector();
+    JScrollPane scrollPane;
 
     public View(){
         setTitle("Results");
         setSize(600, 600);
         setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
 
+        column_names.add("Номер");
+        column_names.add("Дата");
+        column_names.add("Сумма");
+        column_names.add("Тип");
+
+        resultTable = new JTable(new Vector(), column_names);
+
         showButton.addActionListener(new Controller(this));
 
-        JPanel myPanel = new JPanel(new FlowLayout(FlowLayout.LEFT));
+        JPanel myPanel = new JPanel(new FlowLayout(FlowLayout.CENTER));
         myPanel.add(showButton);
-        myPanel.add(resultTable);
+        add(myPanel, BorderLayout.NORTH);
 
-        add(myPanel);
+        scrollPane = new JScrollPane(resultTable);
+        add(scrollPane, BorderLayout.CENTER);
+
         setVisible(true);
 
     }
@@ -30,5 +42,9 @@ public class View extends JFrame {
 
     public void setResultTable(JTable resultTable) {
         this.resultTable = resultTable;
+    }
+
+    public Vector getColumn_names() {
+        return column_names;
     }
 }
